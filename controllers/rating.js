e39b0eb75c297b3472699c78750feba387e9ab7c
mhdAzhar <azhartk20@gmail.com>
2023-08-03 21:17:06 +0700

    Perbaikan controller rating untuk mengambil data kategori rating

--- controllers/rating.js
+++ controllers/rating.js
@@ -1,7 +1,7 @@
 const db = require("../configs/database");
 
 exports.getAllRating = async () => {
-    const query = await db.query("SELECT r.IdRating, r.TanggalUpload, r.Ulasan, r.Bintang, p.UrlGambar, p.Nama FROM rating as r INNER JOIN pengguna as p on r.IdPengguna = p.IdPengguna ORDER BY r.IdRating DESC")
+    const query = await db.query("SELECT r.IdRating, r.TanggalUpload, r.Ulasan, r.Bintang, r.KategoriRating, p.UrlGambar, p.Nama FROM rating as r INNER JOIN pengguna as p on r.IdPengguna = p.IdPengguna ORDER BY r.IdRating DESC")
     return query
 };
 
